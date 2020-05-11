0d0b0d30c85d5d260fc07de81679249334ba94d7
Senya0ne <liketravelin@gmail.com>
2020-05-11 20:09:29 +0300

    Dz8 intro selenium (#1)
    * add changes for .gitignore
    
    * add test for Chromedriver
    
    * add changes for fixture init webdrivers
    
    * add url param fixture and option
    
    * refactoring conftest and test_main_page, add changes for pylint
    
    * refactoring conftest and remove browser.quit from test
    
    * add changes for architecture project
    
    * add requests for requirements
    
    * updates for pytest and requests

---
+++ conftest.py
@@ -0,0 +1,55 @@
+""" string for pylint"""
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    """Параметры для запуска тестов"""
+
+    parser.addoption('--browser',
+                     action='store',
+                     default='chrome',
+                     help='Передайте драйвер браузера с помощью параметра --browser, '
+                          'доступны safari, firefox, chrome'
+                          ', например, --browser=safari')
+
+    parser.addoption('--url',
+                     action='store',
+                     default='http://localhost',
+                     help='Передайте url с помощью параметра --url,'
+                          'например, --url=https://yandex.ru')
+
+
+@pytest.fixture
+def url_param(request):
+    """Фикстура для перадачи url"""
+    return request.config.getoption('--url')
+
+
+@pytest.fixture()
+def web_driver(request):
+    """
+    Фикстура для инициализации и передачи драйверов
+    """
+
+    browser = request.config.getoption('--browser')
+
+    if browser == 'firefox':
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        driver.maximize_window()
+    elif browser == 'chrome':
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        driver = webdriver.Chrome(options=options)
+        driver.maximize_window()
+    elif browser == 'safari':
+        driver = webdriver.Safari()
+        driver.maximize_window()
+
+    def driver_finalizer():
+        driver.quit()
+
+    request.addfinalizer(driver_finalizer)
+    return driver
