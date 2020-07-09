d78b72b719346d3e4b6519502a3a4fbf27dbf9ba
senya0ne <liketravelin@gmail.com>
2020-07-09 23:33:19 +0300

    add waits for tests

--- tests/test_admin_login_page.py
+++ tests/test_admin_login_page.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from POM.locators import AdminLocators
 from selenium.webdriver.common.by import By
 
@@ -14,12 +16,26 @@ def test_title(browser, base_url):
 
 def test_auth_success(browser, base_url):
     browser.get(base_url + url_helper)
+    wait = WebDriverWait(browser, 3)
     browser.find_element(By.ID, AdminLocators.INPUT_USER).click()
     browser.find_element(By.ID, AdminLocators.INPUT_USER).send_keys(user)
     browser.find_element(By.ID, AdminLocators.INPUT_PWD).click()
     browser.find_element(By.ID, AdminLocators.INPUT_PWD).send_keys(password)
     browser.find_element(By.CSS_SELECTOR, AdminLocators.BTN_LOGIN_ADMIN).click()
-    assert "Dashboard" in browser.title
+    wait.until(EC.title_is("Dashboard"))
+
+
+def test_login_and_logout(browser, base_url):
+    browser.get(base_url + url_helper)
+    wait = WebDriverWait(browser, 3)
+    browser.find_element(By.ID, AdminLocators.INPUT_USER).click()
+    browser.find_element(By.ID, AdminLocators.INPUT_USER).send_keys(user)
+    browser.find_element(By.ID, AdminLocators.INPUT_PWD).click()
+    browser.find_element(By.ID, AdminLocators.INPUT_PWD).send_keys(password)
+    browser.find_element(By.CSS_SELECTOR, AdminLocators.BTN_LOGIN_ADMIN).click()
+    wait.until(EC.title_is("Dashboard"))
+    browser.find_element(By.CSS_SELECTOR, AdminLocators.BTN_LOGOUT).click()
+    wait.until(EC.title_is("Administration"))
 
 
 def test_auth_failed(browser, base_url):
@@ -46,5 +62,5 @@ def test_forgotten_password(browser, base_url):
     browser.find_element(By.ID, AdminLocators.EMAIL_FIELD).click()
     browser.find_element(By.ID, AdminLocators.EMAIL_FIELD).send_keys(email)
     browser.find_element(By.CSS_SELECTOR, AdminLocators.BTN_LOGIN_ADMIN).click()
-    assert "An email with a confirmation link has been sent your admin email address." in browser.find_element(By.CLASS_NAME, AdminLocators.ALERT).text
-
+    assert "An email with a confirmation link has been sent your admin email address." in browser.find_element(
+        By.CLASS_NAME, AdminLocators.ALERT).text
